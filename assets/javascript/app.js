8a662f1ef3c1824607112dec82c125242106d15a
Mark <contact.mshin@gmail.com>
2018-10-08 18:07:02 -0600

    html finished

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyB5pRVp3tUB4XWxFXoxDfrtZileKBkJwSY",
+authDomain: "train-homework-24bf1.firebaseapp.com",
+databaseURL: "https://train-homework-24bf1.firebaseio.com",
+projectId: "train-homework-24bf1",
+storageBucket: "",
+messagingSenderId: "40239820778"
+};
+firebase.initializeApp(config);
+
+//Global Variables
